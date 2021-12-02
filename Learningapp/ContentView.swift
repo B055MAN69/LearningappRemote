f04b768cd68ff18c1052cf24725f04d6c75cea65
B055MAN69 <tristanbutler669@gmail.com>
2021-12-02 12:04:09 -0600

    Initial Commit

---
+++ Learningapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learningapp
+//
+//  Created by PM Ada - Tristan Butler on 12/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learningapp/LearningappApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningappApp.swift
+//  Learningapp
+//
+//  Created by PM Ada - Tristan Butler on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
